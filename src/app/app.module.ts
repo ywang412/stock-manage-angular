d552e25c52dd11dd2b99faa24e34ac2b41c8e193
zhangxishuo <zhangxishuo1998@gmail.com>
2018-11-05 21:30:15 +0800

    页面组件拆分

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {HeaderComponent} from './component/header/header.component';
+import {MenuComponent} from './component/menu/menu.component';
+import {SidebarComponent} from './component/sidebar/sidebar.component';
+import {FooterComponent} from './component/footer/footer.component';
+import {ContentComponent} from './component/content/content.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        MenuComponent,
+        SidebarComponent,
+        FooterComponent,
+        ContentComponent
     ],
     imports: [
         BrowserModule
